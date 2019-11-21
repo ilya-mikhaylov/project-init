fa5e06b20a08015e7bc55e77f4af2e6f16b6be13
Ilya Mikhaylov <doxxmsk@gmail.com>
2019-11-22 01:15:11 +0300

    Code comments added

--- init.sh
+++ init.sh
@@ -1,16 +1,27 @@
 echo '// ProjectInit ver. 0.9 //'
+echo 'https://github.com/ilya-mikhaylov/project-init'
 echo ''
 echo 'This script will download ESLint Airbnb config, create .gitignore and .eslintrc.js in your project folder.'
 echo ''
-read -p 'Create Jasmine tests? Y/N...' prompt
-echo 'Jasmine tests will be also created.'
-if [[ $prompt == "y" || $prompt == "Y"  ]]
+
+# 'read' lets user to set the value of '$prompt' variable.
+
+read -p 'It also can create an empty Jasmine tests. Should we need them? <Y/N>' prompt
+
+# Condition that checks value of '$prompt' variable.
+
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" ]]
 then
+echo 'Good! Jasmine tests will be also created.'
 wget https://github.com/ilya-mikhaylov/project-init/raw/master/tests.tar -O - | tar -x
+else
+echo 'OK, continuing without tests.'
 fi
 
 npm init -yes
 npm install eslint eslint-config-airbnb-base eslint-plugin-import -D
+
+# '>>' option of 'cat' adds fragment between two EOFs to the end to .gitignore. Creates new one, if it doesn't exist.
 
 cat >> .gitignore << EOF
 *.log
