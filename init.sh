c52b8cfd203025ad36db4572a25585db9ba84ff7
Ilya Mikhaylov <doxxmsk@gmail.com>
2019-11-22 01:04:31 +0300

    Version 0.9

--- init.sh
+++ init.sh
@@ -2,14 +2,12 @@ echo '// ProjectInit ver. 0.9 //'
 echo ''
 echo 'This script will download ESLint Airbnb config, create .gitignore and .eslintrc.js in your project folder.'
 echo ''
-read -p 'Create Jasmine tests? Y/N...' -n 1 -r
+read -p 'Create Jasmine tests? Y/N...' prompt
 echo 'Jasmine tests will be also created.'
-# if [ $REPLY =- ^[Yy]$ ]
-# then
-wget https://drive.google.com/$typettps://drive.google.com/open?id=1zBEmUFWrZ-GpMRKd7LxYc7-AEnaALap
-# wget https://drive.google.com/open?id=1GyepAUi_fV0g-xwrzaYeQyIhoxWY4Moa
-# wget https://drive.google.com/open?id=10GQpJkOsnZDnTzRXz7QH6PeZCsay8HDS
-# fi
+if [[ $prompt == "y" || $prompt == "Y"  ]]
+then
+wget https://github.com/ilya-mikhaylov/project-init/raw/master/tests.tar -O - | tar -x
+fi
 
 npm init -yes
 npm install eslint eslint-config-airbnb-base eslint-plugin-import -D
@@ -31,9 +29,4 @@ module.exports = {
 }
 EOF
 
-
-
 echo 'All done.'
-# //rm file.sh
-# /---
-# wget -O https://...
